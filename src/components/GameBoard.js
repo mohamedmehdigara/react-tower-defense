7d28831355a248daef37f1344b0d0432bae10848
mohamedmehdigara <garamehdi57@yahoo.com>
2023-07-06 18:54:15 +0100

    added files

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -26,28 +26,21 @@ const GameBoard = () => {
     const towerGeometry = new THREE.BoxGeometry(1, 1, 1);
     const towerMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
     const tower = new THREE.Mesh(towerGeometry, towerMaterial);
+    tower.position.x = -2;
     scene.add(tower);
+
+    // Create a path
+    const pathGeometry = new THREE.BoxGeometry(4, 0.1, 1);
+    const pathMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+    const path = new THREE.Mesh(pathGeometry, pathMaterial);
+    scene.add(path);
 
     // Create enemies
     const enemyGeometry = new THREE.SphereGeometry(0.5, 32, 32);
     const enemyMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     const enemy = new THREE.Mesh(enemyGeometry, enemyMaterial);
-    enemy.position.x = 3;
+    enemy.position.x = 2;
     scene.add(enemy);
-
-    // Create a path
-    const pathPoints = [
-      new THREE.Vector3(-2, 0, 0),
-      new THREE.Vector3(-1, 0, 0),
-      new THREE.Vector3(0, 0, 1),
-      new THREE.Vector3(1, 0, 0),
-      new THREE.Vector3(2, 0, 0),
-    ];
-    const pathSpline = new THREE.CatmullRomCurve3(pathPoints);
-    const pathGeometry = new THREE.TubeGeometry(pathSpline, 64, 0.1, 8, false);
-    const pathMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-    const path = new THREE.Mesh(pathGeometry, pathMaterial);
-    scene.add(path);
 
     // Animate the scene
     const animate = () => {
@@ -57,19 +50,8 @@ const GameBoard = () => {
       tower.rotation.x += 0.01;
       tower.rotation.y += 0.01;
 
-      // Move enemy along the path
-      if (
-        pathGeometry &&
-        pathGeometry.parameters &&
-        pathGeometry.parameters.path &&
-        pathGeometry.parameters.path.curves &&
-        pathGeometry.parameters.path.curves[0]
-      ) {
-        const pointOnPath = pathGeometry.parameters.path.curves[0].getPoint(
-          (enemy.position.x + 2) / 4
-        );
-        enemy.position.copy(pointOnPath);
-      }
+      // Move enemy
+      enemy.position.x -= 0.01;
 
       // Render the scene
       renderer.render(scene, camera);
@@ -79,7 +61,9 @@ const GameBoard = () => {
 
     // Clean up
     return () => {
-      containerRef.current.removeChild(renderer.domElement);
+      if (containerRef.current) {
+        containerRef.current.removeChild(renderer.domElement);
+      }
     };
   }, []);
 
