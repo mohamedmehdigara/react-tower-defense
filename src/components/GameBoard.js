50bc5497b89f30fdc121aadd5618ebcc9f3a1cf9
mohamedmehdigara <garamehdi57@yahoo.com>
2023-10-03 10:31:45 +0100

    edited files

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -5,69 +5,70 @@ const GameBoard = () => {
   const containerRef = useRef(null);
 
   useEffect(() => {
-    // Create a scene
     const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(0, 2, 8);
 
-    // Create a camera
-    const camera = new THREE.PerspectiveCamera(
-      75, // Field of view
-      window.innerWidth / window.innerHeight, // Aspect ratio
-      0.1, // Near clipping plane
-      1000 // Far clipping plane
-    );
-    camera.position.set(0, 2, 8); // Adjust camera position
-
-    // Create a renderer
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     containerRef.current.appendChild(renderer.domElement);
 
-    // Create a tower
-    const towerGeometry = new THREE.BoxGeometry(1, 3, 1); // Adjust tower size
-    const towerMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000 }); // Use standard material for better lighting
-    const tower = new THREE.Mesh(towerGeometry, towerMaterial);
-    tower.position.x = -2;
-    scene.add(tower);
+    // Arrays to hold towers and enemies
+    const towers = [];
+    const enemies = [];
 
-    // Create a path
+    // Create and position towers
+    const towerGeometry = new THREE.BoxGeometry(1, 3, 1);
+    const towerMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+    for (let i = 0; i < 5; i++) {
+      const tower = new THREE.Mesh(towerGeometry, towerMaterial);
+      tower.position.set(-3 + i * 1.5, 1.5, 0); // Adjust tower positions
+      towers.push(tower);
+      scene.add(tower);
+    }
+
+    // Create path
     const pathGeometry = new THREE.BoxGeometry(4, 0.1, 1);
-    const pathMaterial = new THREE.MeshStandardMaterial({ color: 0x0000ff }); // Use standard material
+    const pathMaterial = new THREE.MeshStandardMaterial({ color: 0x0000ff });
     const path = new THREE.Mesh(pathGeometry, pathMaterial);
     scene.add(path);
 
-    // Create enemies
+    // Create and position enemies
     const enemyGeometry = new THREE.SphereGeometry(0.5, 32, 32);
-    const enemyMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 }); // Use standard material
-    const enemy = new THREE.Mesh(enemyGeometry, enemyMaterial);
-    enemy.position.x = 2;
-    scene.add(enemy);
+    const enemyMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+    for (let i = 0; i < 10; i++) {
+      const enemy = new THREE.Mesh(enemyGeometry, enemyMaterial);
+      enemy.position.set(4 + i * 2, 0.5, 0); // Adjust enemy positions
+      enemies.push(enemy);
+      scene.add(enemy);
+    }
 
-    // Add lighting
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // Ambient light
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
     scene.add(ambientLight);
 
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5); // Directional light
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
     directionalLight.position.set(0, 5, 5);
     scene.add(directionalLight);
 
-    // Animate the scene
     const animate = () => {
       requestAnimationFrame(animate);
 
-      // Rotate tower
-      tower.rotation.x += 0.005;
-      tower.rotation.y += 0.005;
+      towers.forEach((tower) => {
+        tower.rotation.x += 0.005;
+        tower.rotation.y += 0.005;
+        // Implement tower logic here (e.g., targeting enemies)
+      });
 
-      // Move enemy along the path
-      enemy.position.x -= 0.01;
+      enemies.forEach((enemy) => {
+        enemy.position.x -= 0.01;
+        // Implement enemy logic here (e.g., collision detection with towers)
+      });
 
-      // Render the scene
       renderer.render(scene, camera);
     };
 
     animate();
 
-    // Handle window resize
     const handleResize = () => {
       const newWidth = window.innerWidth;
       const newHeight = window.innerHeight;
@@ -78,7 +79,6 @@ const GameBoard = () => {
 
     window.addEventListener('resize', handleResize);
 
-    // Clean up
     return () => {
       if (containerRef.current) {
         containerRef.current.removeChild(renderer.domElement);
@@ -91,3 +91,4 @@ const GameBoard = () => {
 };
 
 export default GameBoard;
+
