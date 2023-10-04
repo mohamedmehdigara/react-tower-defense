8d684233609dede0c75bb87c9f3bdc362e3c2f58
mohamedmehdigara <garamehdi57@yahoo.com>
2023-10-04 12:12:05 +0100

    edited files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import GameBoard from './components/GameBoard';
 import Enemy from './components/Enemy';
 import Tower from './components/Tower';
 import * as THREE from 'three'; // Import Three.js
 
 const App = () => {
-  // Create a Three.js scene
-  const scene = new THREE.Scene();
+  // Initialize scene, camera, and renderer as states
+  const [scene, setScene] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [renderer, setRenderer] = useState(null);
 
+  useEffect(() => {
+    console.log("Initializing Three.js");
+
+  // ... (Three.js initialization code)
+
+  console.log("Three.js initialization complete");
+    // Create a Three.js scene, camera, and renderer
+    const newScene = new THREE.Scene();
+    const newCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const newRenderer = new THREE.WebGLRenderer();
+
+    // Set initial positions and properties for camera and renderer
+    newCamera.position.set(0, 2, 8);
+    newRenderer.setSize(window.innerWidth, window.innerHeight);
+
+    // Append the renderer's DOM element to the container
+    document.getElementById('game-container').appendChild(newRenderer.domElement);
+
+    // Set states for scene, camera, and renderer
+    setScene(newScene);
+    setCamera(newCamera);
+    setRenderer(newRenderer);
+
+    // Clean up on unmount
+    return () => {
+      if (newRenderer) {
+        newRenderer.dispose();
+      }
+    };
+  }, []);
   return (
     <div className="App">
-      <GameBoard scene={scene} /> {/* Pass the scene to GameBoard */}
+      <GameBoard scene={scene} camera={camera} renderer={renderer}/> {/* Pass the scene to GameBoard */}
       <Enemy
         position={0}
         path={[0, 1, 2, 3, 4]} // Replace with your actual path array
