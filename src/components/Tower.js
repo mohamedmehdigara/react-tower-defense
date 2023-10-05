a246afea1d0af028b9c3e64ca236ec2ba9eb00b9
mohamedmehdigara <garamehdi57@yahoo.com>
2023-10-05 16:23:52 +0100

    edited files

--- src/components/Tower.js
+++ src/components/Tower.js
@@ -6,64 +6,69 @@ const Tower = ({ position, target }) => {
 
   useEffect(() => {
     // Check if the target.scene is available
-    if (target.scene) {
-      // Create tower mesh
-      const towerGeometry = new THREE.BoxGeometry(1, 3, 1);
-      const towerMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000 });
-      const tower = new THREE.Mesh(towerGeometry, towerMaterial);
-      tower.position.set(position.x, position.y, position.z);
-
-      // Add tower to scene
-      target.scene.add(tower);
-
-      // Define tower attributes
-      const attackDamage = 10; // Adjust damage as needed
-      const attackCooldown = 1000; // Cooldown in milliseconds
-
-      // Function to handle tower attacks
-      const attack = () => {
-        if (target.enemies && Array.isArray(target.enemies) && cooldown <= 0 && target.enemies.length > 0) {
-          // Find the closest enemy
-          let closestEnemy = null;
-          let closestDistance = Infinity;
-      
-          target.enemies.forEach((enemy) => {
-            const distance = tower.position.distanceTo(enemy.position);
-            if (distance < closestDistance) {
-              closestDistance = distance;
-              closestEnemy = enemy;
-            }
-          });
-      
-          if (closestEnemy) {
-            // Attack the closest enemy
-            closestEnemy.takeDamage(attackDamage);
-            setCooldown(attackCooldown);
-          }
-        }
-      };
-      
-      // Animation loop for tower attacks
-      const animate = () => {
-        requestAnimationFrame(animate);
-
-        // Reduce cooldown
-        if (cooldown > 0) {
-          setCooldown(cooldown - 1);
-        }
-
-        // Tower logic - implement your targeting and attack logic here
-        attack();
-      };
-
-      animate();
-
-      // Clean up when unmounted
-      return () => {
-        target.scene.remove(tower);
-      };
+    if (!target.scene) {
+      return;
     }
-  }, [position, target]);
+
+    // Create tower mesh
+    const towerGeometry = new THREE.BoxGeometry(1, 3, 1);
+    const towerMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+    const tower = new THREE.Mesh(towerGeometry, towerMaterial);
+    tower.position.copy(position);
+
+    // Add tower to scene
+    target.scene.add(tower);
+
+    // Define tower attributes
+    const attackDamage = 10; // Adjust damage as needed
+    const attackCooldown = 1000; // Cooldown in milliseconds
+
+    // Function to handle tower attacks
+   // Function to handle tower attacks
+const attack = () => {
+  if (!target.enemies || !Array.isArray(target.enemies) || cooldown > 0 || target.enemies.length === 0) {
+    return; // No valid enemies to attack
+  }
+
+  // Find the closest enemy
+  let closestEnemy = null;
+  let closestDistance = Infinity;
+
+  target.enemies.forEach((enemy) => {
+    const distance = tower.position.distanceTo(enemy.position);
+    if (distance < closestDistance) {
+      closestDistance = distance;
+      closestEnemy = enemy;
+    }
+  });
+
+  if (closestEnemy) {
+    // Attack the closest enemy
+    closestEnemy.takeDamage(attackDamage);
+    setCooldown(attackCooldown);
+  }
+};
+
+    // Animation loop for tower attacks
+    const animate = () => {
+      requestAnimationFrame(animate);
+
+      // Reduce cooldown
+      if (cooldown > 0) {
+        setCooldown(cooldown - 1);
+      }
+
+      // Tower logic - implement your targeting and attack logic here
+      attack();
+    };
+
+    animate();
+
+    // Clean up when unmounted
+    return () => {
+      target.scene.remove(tower);
+    };
+  }, [position, target, cooldown]);
 
   return null; // Towers are managed in the Three.js scene
 };
