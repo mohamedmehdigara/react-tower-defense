19ce5a47729c31fd84288192f9b59a76eabf4ea4
mohamedmehdigara <garamehdi57@yahoo.com>
2023-10-10 10:35:20 +0100

    edited files

--- src/components/Tower/Tower.js
+++ src/components/Tower/Tower.js
@@ -2,22 +2,28 @@ import React, { useEffect, useState, useRef } from 'react';
 import * as THREE from 'three';
 
 const Tower = ({ position, target }) => {
+  // State to track the cooldown for tower attacks
   const [cooldown, setCooldown] = useState(0);
+
+  // Ref to store arrows fired by the tower
   const arrows = useRef([]);
-  const towerRef = useRef(null); // Add a reference for the tower mesh
+
+  // Ref to store the tower mesh for cleanup on unmount
+  const towerRef = useRef(null);
 
   useEffect(() => {
     if (!target.scene) {
       return;
     }
 
-    // Constants
+    // Constants for tower and arrow properties
     const TOWER_COLOR = 0xff0000;
     const ARROW_COLOR = 0x00ff00;
     const ATTACK_DAMAGE = 10;
     const ATTACK_COOLDOWN = 1000;
     const ARROW_SPEED = 0.05;
 
+    // Function to create the tower mesh and add it to the scene
     const createTower = () => {
       const towerGeometry = new THREE.BoxGeometry(1, 3, 1);
       const towerMaterial = new THREE.MeshStandardMaterial({ color: TOWER_COLOR });
@@ -25,9 +31,10 @@ const Tower = ({ position, target }) => {
       tower.position.copy(position);
 
       target.scene.add(tower);
-      towerRef.current = tower; // Set the tower reference
+      towerRef.current = tower;
     };
 
+    // Function to create an arrow and add it to the scene
     const createArrow = (direction) => {
       const arrowGeometry = new THREE.BoxGeometry(0.1, 0.1, 1);
       const arrowMaterial = new THREE.MeshStandardMaterial({ color: ARROW_COLOR });
@@ -37,6 +44,7 @@ const Tower = ({ position, target }) => {
       target.scene.add(arrow);
     };
 
+    // Function to handle tower attacks
     const attack = () => {
       if (!target.enemies || !Array.isArray(target.enemies) || cooldown > 0 || target.enemies.length === 0) {
         return;
@@ -45,8 +53,13 @@ const Tower = ({ position, target }) => {
       let closestEnemy = null;
       let closestDistance = Infinity;
 
+      // Define towerPosition here in the same scope
+      const towerPosition = new THREE.Vector3(position.x, position.y, position.z);
+
       target.enemies.forEach((enemy) => {
-        const distance = position.distanceTo(enemy.position);
+        const enemyPosition = new THREE.Vector3(enemy.position.x, enemy.position.y, enemy.position.z);
+
+        const distance = towerPosition.distanceTo(enemyPosition);
         if (distance < closestDistance) {
           closestDistance = distance;
           closestEnemy = enemy;
@@ -54,13 +67,14 @@ const Tower = ({ position, target }) => {
       });
 
       if (closestEnemy) {
-        const direction = new THREE.Vector3().subVectors(closestEnemy.position, position).normalize();
+        const direction = new THREE.Vector3().subVectors(closestEnemy.position, towerPosition).normalize();
         createArrow(direction);
 
         setCooldown(ATTACK_COOLDOWN);
       }
     };
 
+    // Function to animate the arrows and check for enemy hits
     const animateArrows = () => {
       arrows.current.forEach(({ arrow, direction }) => {
         arrow.position.add(direction.clone().multiplyScalar(ARROW_SPEED));
@@ -80,6 +94,7 @@ const Tower = ({ position, target }) => {
       requestAnimationFrame(animateArrows);
     };
 
+    // Main animation loop for tower attacks and cooldown
     const animate = () => {
       requestAnimationFrame(animate);
 
@@ -90,20 +105,21 @@ const Tower = ({ position, target }) => {
       attack();
     };
 
-    // Create tower and start animations
+    // Initial tower setup and animation
     createTower();
     animate();
     animateArrows();
 
+    // Cleanup function to remove tower and arrows from the scene
     return () => {
-      // Clean up tower and arrows when component unmounts
-      target.scene.remove(towerRef.current); // Use the tower reference
+      target.scene.remove(towerRef.current);
       arrows.current.forEach(({ arrow }) => {
         target.scene.remove(arrow);
       });
     };
   }, [position, target, cooldown]);
 
+  // Return null as this component does not render any visible elements
   return null;
 };
 
